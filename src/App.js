56f4c52d7d28c21fdfcef5668b2dfe90176d278a
Cynthia <103306002@nccu.edu.tw>
2022-04-03 22:48:59 -0500

    firebase setting

--- src/App.js
+++ src/App.js
@@ -3,74 +3,7 @@ import React,{useState,useEffect} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import User from './components/users.js'
 import { UserInfoList } from './components/users.js';
-
-
-
-const userInfo = {
-  users: [
-{
-    "name": "Josh Breite",
-    "phoneNumber": "212-111-1111",
-    "pictures" : "https://thegarage.northwestern.edu/wp-content/uploads/2021/09/Josh-Breite-Headshot.jpg",
-    "projects" : [
-       { "name" : "Induction Learning",
-        "projectDescription" : "Reimaning the education landscape.",
-        "teamSize" : 6,
-        "projectDuration": "2 years",
-        "greatestMoment": "Deploying the app.",
-        "projectLink" : "https://www.inductionlearning.com/"
-        },
-      { "name" : "EntrepMatch",
-        "projectDescription" : "Find your Cofounders.",
-        "teamSize" : 8,
-        "projectDuration": "1 week",
-        "greatestMoment": "Building in React.",
-        "projectLink" : "https://www.ycombinator.com/"
-      }
-    ],
-    "favoriteEntreprenuer" : "Satoshi Nakamoto",
-    "industryInterest" : ["Crypto", "FinTech"],
-    "school" : "Northwestern",
-    "major" : "Computer Science",
-    "skills" : {
-      "artistic" : [],
-      "technical" : ["React", "Python", "SQL"],
-      "softSkills" : ["Organization", "Notion"]
-      } 
-    },
-
-    {
-      "name": "Elon Musk",
-      "phoneNumber": "212-333-4444",
-      "pictures" : "https://cdn.britannica.com/45/223045-050-A6453D5D/Telsa-CEO-Elon-Musk-2014.jpg",
-      "projects" : [
-        { "name" : "Tesla",
-          "projectDescription" : "Electric Vehicles.",
-          "teamSize" : 3000,
-          "projectDuration": "4 years",
-          "greatestMoment": "Sustainable World.",
-          "projectLink" : "https://www.tesla.com/"
-          },
-        { "name" : "PayPal",
-          "projectDescription" : "Pay people online.",
-          "teamSize" : 5000,
-          "projectDuration": "10 years",
-          "greatestMoment": "First online payment.",
-          "projectLink" : "https://www.paypal.com/us/home/"
-        }
-      ],
-      "favoriteEntreprenuer" : "Elon Musk",
-      "industryInterest" : ["Crypto", "FinTech", "Sustainables"],
-      "school" : "University of Pennsylvannia",
-      "major" : "Computer Science",
-      "skills" : {
-        "artistic" : [],
-        "technical" : ["React", "Python", "SQL"],
-        "softSkills" : ["Organization", "Notion", "Leadership"]
-        } 
-      },
-    ] 
-}
+import { useData } from './utilities/firebase.js';
 
 // const UserInfoList = ({ users }) => (
   
@@ -82,6 +15,11 @@ const userInfo = {
 // );
 
 function App() {
+  const [userInfo, loading, error] = useData('/'); 
+  
+  if (error) return <h1>{error}</h1>;
+  if (loading) return <h1>Loading...</h1>
+
   console.log(userInfo.users);
   console.log(userInfo.projects)
   
