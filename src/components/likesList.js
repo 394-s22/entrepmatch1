772df42c29008d9077eaa782447964bfb35b92e9
joshBreite <joshuabreite2022@u.northwestern.edu>
2022-04-10 17:27:44 -0500

    Update likesList.js

--- src/components/likesList.js
+++ src/components/likesList.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { Button } from 'react-bootstrap';
+import { Button, Modal } from 'react-bootstrap';
 import Card from 'react-bootstrap/Card';
+import User from '../components/users.js'
 
 
  const idLikes = ({ user }) => (
@@ -40,7 +41,25 @@ import Card from 'react-bootstrap/Card';
         <Card.Title> { user[user.findIndex(id => id.user_id === idLikesArray()[0])].name} </Card.Title>
         <Card.Subtitle> { user[user.findIndex(id => id.user_id === idLikesArray()[0])].school} </Card.Subtitle>
         <Card.Text> Need to get the message here... Not sure what to do </Card.Text>
+        <>
         <Button variant="primary" onClick={handleShow}>Show Profile</Button>
+        <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+              <Modal.Title> contact Information </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>Contact  </Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={handleClose}>
+                Close
+              </Button>
+              <Button variant="primary" onClick={() => {
+                setShow(false);
+                }}>
+                Next Profile
+              </Button>
+            </Modal.Footer>
+          </Modal>
+        </>
         </Card.Body>
     </Card>
     )
