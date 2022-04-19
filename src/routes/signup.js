b1bd275e07a56e8273578d67a0b5189f0077880d
notfingees <75046362+notfingees@users.noreply.github.com>
2022-04-19 11:39:31 -0500

    Multiple projects, skills, interests functionality in signup

--- src/routes/signup.js
+++ src/routes/signup.js
@@ -10,11 +10,58 @@ import { useData, signInWithGoogle, signOut, useUserState, setData, pushData } f
 import { Link } from "react-router-dom";
 import Avatar from 'react-avatar';
 
+var num_projects = 1;
+const AddProjectForm = (_num_projects) => (
+    
+  <div class="add_project_form">
+  <input type="text" id={'project_name' + _num_projects} placeholder="project name"></input>
+    
+    <input type="text" id={'team_size' + _num_projects} placeholder="team size"></input>
+    <input type="text" id={"project_duration" + _num_projects} placeholder="project duration"></input>
+    <input type="text" id={"greatest_moment" + _num_projects} placeholder="greatest moment"></input>
+    <input type="text" id={"project_link" + _num_projects} placeholder="project link"></input>
+    <input type="text" id={"project_description" + _num_projects} placeholder="project description"></input>
+    </div>
+);
+
+var projectsArray = [AddProjectForm(1)]
+
 export default function SignUp() {
+
+
+  const [, updateState] = React.useState();
+ const forceUpdate = React.useCallback(() => updateState({}), []);
+
+
+
   
   const [userInfo, loading, error] = useData('/');
   const [user] = useUserState();
   console.log(user);
+
+  
+
+  const [name, setName] = useState('Mary');
+  
+
+  function update_num_projects(){
+    
+    console.log("in update_num_projects")
+    num_projects += 1;
+    projectsArray.push(AddProjectForm(num_projects));
+
+    console.log("projectsArray is now", projectsArray, "num_projects is", num_projects)
+    forceUpdate();
+    
+    
+  }
+
+  
+
+  
+
+
+
 
   const current_user_id = 0 // need to update this after testing to be the current user
   if (error) return <h1>{error}</h1>;
@@ -28,29 +75,35 @@ export default function SignUp() {
   // Here is where we push the data
   //Once confident about the right format, remove the /3/ and let it oush to users
   try {
+
+    var _projects = []
+    for (let i = 0; i < num_projects; i++) {
+      var new_project = {
+        greatestMoment: document.getElementById("greatest_moment"+i.toString()).value,
+        name: document.getElementById("project_name"+i.toString()).value,
+          projectDescription: document.getElementById("project_description"+i.toString()).value,
+          
+          projectDuration: document.getElementById("project_duration"+i.toString()).value,
+          projectLink: document.getElementById("project_link"+i.toString()).value,
+          teamSize: document.getElementById("team_size"+i.toString()).value
+
+      }
+      _projects.push(new_project)
+    }
       pushData(`/users/`, {
         conversations:"",
         favoriteEntreprenuer: document.getElementById("favoriteEntreprenuer").value,
-        industryInterest: document.getElementById("industryInterest").value,
+        industryInterest: document.getElementById("industryInterest").value.split(","),
         major: document.getElementById("major").value,
         name: document.getElementById("fullname").value,
         phoneNumber: document.getElementById("phoneNumber").value,
         pictures: document.getElementById("pictures").value,
-        projects: [{
-          greatestMoment: document.getElementById("greatest moment").value,
-          name: document.getElementById("project name").value,
-          school: document.getElementById("school").value,
-          projectDescription: document.getElementById("project description").value,
-          
-          projectDuration: document.getElementById("project duration").value,
-          projectLink: document.getElementById("project link").value,
-          teamSize: document.getElementById("team size").value
-        }],
+        projects: _projects,
         school: document.getElementById("school").value,
-        "skills" : {
-          "artistic" : ["design"],
-          "technical" : ["React", "Python", "SQL"],
-          "softSkills" : ["Organization", "Notion"]
+        skills: {
+          artistic: document.getElementById("artistic_skills").value.split(","),
+          technical: document.getElementById("technical_skills").value.split(","),
+          soft: document.getElementById("soft_skills").value.split(","),
         },
         liked_users : [],
         users_liked :[],
@@ -69,6 +122,9 @@ export default function SignUp() {
 
 
   // Insert input text fields
+
+
+
   return (
     <div 
       style={{
@@ -85,29 +141,36 @@ export default function SignUp() {
       <input type="text" id="industryInterest" placeholder="industry interest(can be a list)"></input>
       <input type="text" id="school" placeholder="school"></input>
        */}
+       <h3>About You</h3>
       <input type="text" id="fullname" placeholder ="Full Name" required></input>
       <input type="text" id="pictures" placeholder="pictures"></input>
-
       <input type="text" id="phoneNumber" placeholder="phone Number"></input>
+      <br></br>
+      <h3>Interests</h3>
       <input type="text" id="favoriteEntreprenuer" placeholder="favorite entrepreneur"></input>
-      <input type="text" id="industryInterest" placeholder="industry interest(can be a list)"></input>
+      <input type="text" id="industryInterest" placeholder="industry interest(can be a list seperated by commas)"></input>
       <input type="text" id="school" placeholder="school"></input>
       <input type="text" id="major" placeholder="major"></input>
+      <br></br>
+      <h3>Skills</h3>
+      <input type="text" id="artistic_skills" placeholder="Artistic skills (can be a list seperated by commas)"></input>
+      <input type="text" id="technical_skills" placeholder="Technical skills (can be a list seperated by commas)"></input>
+      <input type="text" id="soft_skills" placeholder="Soft skills (can be a list seperated by commas)"></input>
+      <br></br>
 
-      <input type="text" id="skills" placeholder="skills(can be a list)"></input>
-      <input type="text" id="project name" placeholder="project name"></input>
-      <input type="text" id="team size" placeholder="team size"></input>
-      <input type="text" id="project duration" placeholder="project duration"></input>
-      <input type="text" id="greatest moment" placeholder="greatest moment"></input>
-      <input type="text" id="project link" placeholder="project link"></input>
-      <input type="text" id="project description" placeholder="project description"></input>
+      <h3>Projects</h3>
+      {/*Array(num_projects).fill(null).map((value, index) => (
+        <AddProjectForm key={index}/>
+      ))*/}
 
 
-      {/* <input type="text" id="phonenumber" value=""></input>
-      <input type="text" id="phonenumber" value=""></input>
-      <input type="text" id="phonenumber" value=""></input>
-      <input type="text" id="phonenumber" value=""></input>
-      <input type="text" id="phonenumber" value=""></input> */}
+      {[...Array(num_projects).keys()].map(function(object, i){
+        return AddProjectForm(object);
+    })}
+      
+      <button onClick={update_num_projects}>Add another project</button>
+
+
 
 
 
