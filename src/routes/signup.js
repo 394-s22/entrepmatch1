6d10e58f53e57dce8f8d37d43edbaccc7cee28a9
Cynthia <Liangshao2023@u.northwestern.edu>
2022-04-21 14:49:38 -0500

    upload image to firebase storage

--- src/routes/signup.js
+++ src/routes/signup.js
@@ -6,9 +6,10 @@ import UserLikeList from '../components/likesList';
 
 import '../App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useData, signInWithGoogle, signOut, useUserState, setData, pushData } from '../utilities/firebase.js';
+import { useData, signInWithGoogle, signOut, useUserState, setData, pushData, storage } from '../utilities/firebase.js';
 import { Link } from "react-router-dom";
 import Avatar from 'react-avatar';
+import { ref, uploadBytes, listAll, getDownloadURL } from 'firebase/storage';
 
 var num_projects = 1;
 const AddProjectForm = (_num_projects) => (
@@ -41,7 +42,7 @@ export default function SignUp() {
 
   
 
-  const [name, setName] = useState('Mary');
+  const [imageUpload, setImageUpload] = useState(null);
   
 
   function update_num_projects(){
@@ -56,12 +57,6 @@ export default function SignUp() {
     
   }
 
-  
-
-  
-
-
-
 
   const current_user_id = 0 // need to update this after testing to be the current user
   if (error) return <h1>{error}</h1>;
@@ -71,50 +66,64 @@ export default function SignUp() {
     
     //DO this for all fields
     const message = document.getElementById("fullname").value;
+    var avatarUrl = "";
+    if(imageUpload){
+      const imageRef = ref(storage, `${user.uid}/${imageUpload.name}`);
+      uploadBytes(imageRef, imageUpload).then(() => {
+        const avatarRef = ref(storage, `${user.uid}/`);
+        listAll(avatarRef).then((response) => {
+          getDownloadURL(response.items[0]).then((url) => {
+            avatarUrl = url;
+            // Here is where we push the data
+            //Once confident about the right format, remove the /3/ and let it oush to users
+            try {
+              var _projects = []
+              for (let i = 0; i < num_projects; i++) {
+                var new_project = {
+                  greatestMoment: document.getElementById("greatest_moment"+i.toString()).value,
+                  name: document.getElementById("project_name"+i.toString()).value,
+                    projectDescription: document.getElementById("project_description"+i.toString()).value,
+                    
+                    projectDuration: document.getElementById("project_duration"+i.toString()).value,
+                    projectLink: document.getElementById("project_link"+i.toString()).value,
+                    teamSize: document.getElementById("team_size"+i.toString()).value
 
-  // Here is where we push the data
-  //Once confident about the right format, remove the /3/ and let it oush to users
-  try {
+                }
+                _projects.push(new_project)
+              }
+                pushData(`/users/`, {
+                  conversations:"",
+                  favoriteEntreprenuer: document.getElementById("favoriteEntreprenuer").value,
+                  industryInterest: document.getElementById("industryInterest").value.split(","),
+                  major: document.getElementById("major").value,
+                  name: document.getElementById("fullname").value,
+                  phoneNumber: document.getElementById("phoneNumber").value,
+                  pictures: avatarUrl,
+                  projects: _projects,
+                  school: document.getElementById("school").value,
+                  skills: {
+                    artistic: document.getElementById("artistic_skills").value.split(","),
+                    technical: document.getElementById("technical_skills").value.split(","),
+                    softSkills: document.getElementById("soft_skills").value.split(","),
+                  },
+                  liked_users : [],
+                  users_liked :[],
+                  user_id: user.uid,
+                  }
+                );
+              } catch (error) {
+                alert(error);
+              }
 
-    var _projects = []
-    for (let i = 0; i < num_projects; i++) {
-      var new_project = {
-        greatestMoment: document.getElementById("greatest_moment"+i.toString()).value,
-        name: document.getElementById("project_name"+i.toString()).value,
-          projectDescription: document.getElementById("project_description"+i.toString()).value,
-          
-          projectDuration: document.getElementById("project_duration"+i.toString()).value,
-          projectLink: document.getElementById("project_link"+i.toString()).value,
-          teamSize: document.getElementById("team_size"+i.toString()).value
-
-      }
-      _projects.push(new_project)
+              window.location.href = './profiles';
+          })
+        })
+      })
     }
-      pushData(`/users/`, {
-        conversations:"",
-        favoriteEntreprenuer: document.getElementById("favoriteEntreprenuer").value,
-        industryInterest: document.getElementById("industryInterest").value.split(","),
-        major: document.getElementById("major").value,
-        name: document.getElementById("fullname").value,
-        phoneNumber: document.getElementById("phoneNumber").value,
-        pictures: document.getElementById("pictures").value,
-        projects: _projects,
-        school: document.getElementById("school").value,
-        skills: {
-          artistic: document.getElementById("artistic_skills").value.split(","),
-          technical: document.getElementById("technical_skills").value.split(","),
-          softSkills: document.getElementById("soft_skills").value.split(","),
-        },
-        liked_users : [],
-        users_liked :[],
-        user_id: user.uid,
-        }
-      );
-    } catch (error) {
-      alert(error);
-    }
 
-    window.location.href = './profiles';
+    
+
+  
 
   }
 
@@ -143,7 +152,7 @@ export default function SignUp() {
        */}
        <h3>About You</h3>
       <input type="text" id="fullname" placeholder ="Full Name" required></input>
-      <input type="text" id="pictures" placeholder="pictures"></input>
+      <input type="file" id="pictures" onChange={(event) => {setImageUpload(event.target.files[0])}}></input>
       <input type="text" id="phoneNumber" placeholder="phone Number"></input>
       <br></br>
       <h3>Interests</h3>
