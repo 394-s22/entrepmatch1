7b5fb094699f6485a5a63c97b02304b8f43c3fed
Cynthia <Liangshao2023@u.northwestern.edu>
2022-04-21 20:21:31 -0500

    refactor

--- src/routes/signup.js
+++ src/routes/signup.js
@@ -1,50 +1,33 @@
 import '../App.css';
 import React,{useState,useEffect} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import User from '../components/users.js'
-import UserLikeList from '../components/likesList'; 
-
 import '../App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useData, signInWithGoogle, signOut, useUserState, setData, pushData, storage } from '../utilities/firebase.js';
-import { Link } from "react-router-dom";
-import Avatar from 'react-avatar';
+import { useData, useUserState, pushData, storage } from '../utilities/firebase.js';
 import { ref, uploadBytes, listAll, getDownloadURL } from 'firebase/storage';
 
 var num_projects = 1;
 const AddProjectForm = (_num_projects) => (
     
   <div class="add_project_form">
-  <input type="text" id={'project_name' + _num_projects} placeholder="project name"></input>
-    
+    <input type="text" id={'project_name' + _num_projects} placeholder="project name"></input> 
     <input type="text" id={'team_size' + _num_projects} placeholder="team size"></input>
     <input type="text" id={"project_duration" + _num_projects} placeholder="project duration"></input>
     <input type="text" id={"greatest_moment" + _num_projects} placeholder="greatest moment"></input>
     <input type="text" id={"project_link" + _num_projects} placeholder="project link"></input>
     <input type="text" id={"project_description" + _num_projects} placeholder="project description"></input>
-    </div>
+  </div>
 );
 
 var projectsArray = [AddProjectForm(1)]
 
 export default function SignUp() {
-
-
   const [, updateState] = React.useState();
- const forceUpdate = React.useCallback(() => updateState({}), []);
-
-
-
-  
+  const forceUpdate = React.useCallback(() => updateState({}), []);
   const [userInfo, loading, error] = useData('/');
   const [user] = useUserState();
-  console.log(user);
-
-  
-
   const [imageUpload, setImageUpload] = useState(null);
   
-
   function update_num_projects(){
     
     console.log("in update_num_projects")
@@ -53,10 +36,7 @@ export default function SignUp() {
 
     console.log("projectsArray is now", projectsArray, "num_projects is", num_projects)
     forceUpdate();
-    
-    
   }
-
 
   const current_user_id = 0 // need to update this after testing to be the current user
   if (error) return <h1>{error}</h1>;
@@ -119,21 +99,12 @@ export default function SignUp() {
           })
         })
       })
+    }else{
+      alert("please upload your profile image");
     }
-
-    
-
-  
-
   }
 
-  
-
-
   // Insert input text fields
-
-
-
   return (
     <div 
       style={{
@@ -172,17 +143,11 @@ export default function SignUp() {
         <AddProjectForm key={index}/>
       ))*/}
 
-
       {[...Array(num_projects).keys()].map(function(object, i){
         return AddProjectForm(object);
-    })}
+      })}
       
       <button onClick={update_num_projects}>Add another project</button>
-
-
-
-
-
       <button onClick={sendMessage}>Enter</button>
 
     </div>
