426ee94be1d99ebaf482d65734f01f2a9c77ce6c
jneto26 <jeremy.neto@hotmail.com>
2022-05-18 10:53:39 -0500

    load test

---
+++ cypress/integration/app.spec.js
@@ -0,0 +1,15 @@
+// app.spec.js created with Cypress
+//
+// Start writing your Cypress tests below!
+// If you're unfamiliar with how Cypress works,
+// check out the link below and learn how to write your first test:
+// https://on.cypress.io/writing-first-test
+/* globals cy */
+    
+describe ('Test App', () => {
+
+    it ('launches', () => {
+      cy.visit ('/');
+    });
+  
+  });
