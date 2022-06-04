2560a8f67deaab1dd26f994eabc095b7bf5ebe66
rayamritanshu <amritanshuray2022@u.northwestern.edu>
2022-06-04 12:13:23 -0500

    working on jest setup

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import { useData, useUserState } from '/Users/amritanshuray/Desktop/NUClass/Spring_394_Agile/entrepmatch1/src/utilities/firebase.js';
 import App from './App';
 
 it('shows the micro title', async () => {
@@ -13,3 +14,40 @@ it('shows Sign In if not logged in', async () => {
   const button = await screen.findByText(/Sign In/i);
   expect(button).toBeInTheDocument();
 });
+
+jest.mock('/Users/amritanshuray/Desktop/NUClass/Spring_394_Agile/entrepmatch1/src/utilities/firebase.js');
+
+const mockUserData = {
+  name : "Robbie Waxman"
+};
+
+
+it('logs error if no user', async () => {
+  useData.mockReturnValue([mockUserData, false, null]);
+  useUserState.mockReturnValue([null]);
+  render();
+  const title = 
+  expect(title).toBeInTheDocument();
+});
+
+// export const user_check = () => {
+//   const user = useUserState();
+//   user ? {} : console.error('user not logged in');
+// };
+
+// describe('doing stuff', () => {
+//   it('should do stuff', async () => {
+//     const consoleErrorSpy = jest.spyOn(console, 'error');
+    
+//     user_check();
+    
+//     expect(consoleErrorSpy).toHaveBeenCalledTimes(0); // FAIL
+//   });
+// });
+
+// it('shows Sign Out if logged in', async () => {
+//   useData.mockReturnValue([mockUser, false, null]);
+//   useUserState.mockReturnValue([{ displayName: 'Joe' }]);
+//   render();
+//   const button = await screen.queryByText(/Sign Out/i);
+//   expect(b
